92e69361102b6af901d1ef861626ebedeccceae4
kianakilipaki <kianakilipaki@gmail.com>
2019-06-25 20:56:35 -0600

    lesson-8

--- assignments/lesson-7/js/hamburger.js
+++ assignments/lesson-7/js/hamburger.js
@@ -1,3 +1,9 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+function currentDate() {
+    var today = new Date();
+    
+    document.getElementById("date").innerHTML = x;
+}
--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,9 +1,12 @@
-//get variables from document
-let t = parseInt(document.getElementById("temp").innerHTML);
-let s = parseInt(document.getElementById("speed").innerHTML);
+function windChill() {
+    //get variables from document
+    let t = parseInt(document.getElementById("temp").innerHTML);
+    let s = parseInt(document.getElementById("speed").innerHTML);
 
-//calculate wind chill
-let x = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    //calculate wind chill
+    let x = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
 
-//return windchill
-document.getElementById("wind-chill").innerHTML = x;
+    //return windchill
+    document.getElementById("wind-chill").innerHTML = x;
+
+}
